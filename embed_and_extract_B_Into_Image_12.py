8d0f73555f84d4277f59dea79dfddff2aff9f7ed
yaelpi741 <162157504+yaelpi741@users.noreply.github.com>
2025-06-15 13:08:06 +0300

    2 changed names

--- embed_and_extract_B_Into_Image_12.py
+++ embed_and_extract_B_Into_Image_12.py
@@ -6,7 +6,7 @@ from extract_dh_from_image_2 import extract_dh_from_image_standard_lsb
 from embed_dh_values_into_image_11 import embed_with_standard_lsb_without_AES
 def embed_B_into_image(message, input_image, output_image):
     embed_with_standard_lsb_without_AES(input_image, message, output_image)
-    print(f"✅ DH values ( B ) have been embedded into the image '{output_image}' successfully.")
+    print(f" DH values ( B ) have been embedded into the image '{output_image}' successfully.")
 
 
 def extract_B_from_image(image):
